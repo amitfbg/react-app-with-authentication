1b2a47c40667bb569f625343b47f097ce7f6fe24
Amit Kumar <Amit.Kumar338@T-Mobile.com>
2022-03-05 20:41:03 +0530

    Heroku setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 const bodyParser = require("body-parser");
 const url = require("./config/db-config");
 const auth = require("./middleware/auth");
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 //Database Connection
 mongoose
@@ -31,6 +33,10 @@ const userRoute = require("./routes/user");
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/user", auth, userRoute);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(4000, () => {
   console.log("Server Running on 4000");
