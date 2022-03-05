cf1391566f5e9a7cd2abd96647d3c7d77f90201e
Amit Kumar <Amit.Kumar338@T-Mobile.com>
2022-03-05 21:04:32 +0530

    Revert "Heroku setup"
    This reverts commit 1b2a47c40667bb569f625343b47f097ce7f6fe24.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path");
 const bodyParser = require("body-parser");
 const url = require("./config/db-config");
 const auth = require("./middleware/auth");
@@ -11,7 +10,6 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 //Database Connection
 mongoose
@@ -33,10 +31,6 @@ const userRoute = require("./routes/user");
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/user", auth, userRoute);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 app.listen(4000, () => {
   console.log("Server Running on 4000");
